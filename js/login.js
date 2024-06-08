436268c82c5c5be2c250c83a920454ffd8c1c446
rafaelsaN <rafaelf_santos08@outlook.com>
2024-06-08 20:59:31 -0300

    criando tela de login

---
+++ js/login.js
@@ -0,0 +1,14 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("password");
+    const togglePasswordIcon = document.getElementById("toggle-password");
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      togglePasswordIcon.classList.remove("fa-eye");
+      togglePasswordIcon.classList.add("fa-eye-slash");
+    } else {
+      passwordInput.type = "password";
+      togglePasswordIcon.classList.remove("fa-eye-slash");
+      togglePasswordIcon.classList.add("fa-eye");
+    }
+  }
